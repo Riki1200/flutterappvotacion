1491970f48969d04beec94e219cfd94c6a72a9dc
Romeo <romeo.betances@solvex.com.do>
2021-08-13 16:42:16 -0400

    add flutter and dart files

---
+++ android/app/src/main/kotlin/com/rdev/votaapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rdev.votaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:core';
+import 'package:async/async.dart';
+
+
+int run(int a, int b) {
+  if (a > b) return a;
+  else return b;
+
+}
+
+void main() {
+  runApp(MyApp());
+
+  try {
+    if (Platform.isAndroid || Platform.isIOS) {
+      print("Is Android or Ios");
+    }else {
+      print("is web");
+    }
+  }catch (Exception) {
+    print(Exception.toString());
+  }
+
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
